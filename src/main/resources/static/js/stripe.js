d5dccc8887d126126e8ab1052f6ed6e3add93046
GitHubのユーザー名 <GitHub>
2025-03-27 11:08:25 +0900

    追加しました。

---
+++ src/main/resources/static/js/stripe.js
@@ -0,0 +1,8 @@
+const stripe = Stripe('pk_test_51R6mqrRSOota6fUsZ44ZqeSyzuIwi3uMb2jOeuXCBRbcYz5rolNNpStiFRvz3wsmsJAOhrFjnjMtB7Ji9yK8IkGx00nXaN4nsX');
+const paymentButton = document.querySelector('#paymentButton');
+
+paymentButton.addEventListener('click', () => {
+	stripe.redirectToCheckout({
+		sessionId: sessionId
+	})
+});
